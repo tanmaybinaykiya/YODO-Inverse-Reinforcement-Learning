350f2ecaf36a06f4bc853bf51315ae6ad5e36830
Tanmay Binaykiya <btanmay@gatech.edu>
2018-11-14 16:12:39 -0500

    More fixes

--- 2D/Interface.py
+++ 2D/Interface.py
@@ -1,4 +1,3 @@
-import random
 import time
 
 import numpy as np
@@ -13,92 +12,67 @@ CYAN = (255, 255, 0)
 WHITE = (255, 255, 255)
 COLORS = [RED, BLUE, GREEN, YELLOW, CYAN, WHITE]
 
-actions_taken = {"MOVE": [],
-                 "MOVE_RIGHT": [],
-                 "MOVE_LEFT": [],
-                 "MOVE_UP": [],
-                 "MOVE_DOWN": [],
-                 "PICK": [],
-                 "DROP": [],
-                 "DRAG": [],
-                 "FINISHED": []}
+actions_taken = []
 
 
 class Blocks(pygame.sprite.Sprite):
 
-    def __init__(self, color_index, grid_centers, block_size=50):
+    def __init__(self, color_index, grid_center, block_size=50):
         super(Blocks, self).__init__()
         self.surf = pygame.Surface((block_size, block_size))
         self.surf.fill(COLORS[color_index])
         self.id = color_index
-        self.rect = self.surf.get_rect(center=grid_centers[random.randint(0, len(grid_centers) - 1)])
-
+        self.rect = self.surf.get_rect(center=grid_center)
 
 
 class BlockWorld:
 
-    def __init__(self, screen_width, screen_height, num_blocks=2):
-        self.timer_start = time.time()
+    def __init__(self, screen_width, screen_height, num_blocks=2, record=True):
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         self.master_surface = pygame.Surface(self.screen.get_size())
         self.master_surface.fill((0, 0, 0))
-        self.grid_centers = [(i, 500) for i in range(25, screen_height, 51)]
+        self.grid_centers = [(i, 500) for i in range(25, screen_height, 50)]
         self.blocks = pygame.sprite.Group()
         self.create_blocks(num_blocks)
+        self.record = record
         pygame.init()
-
-        # TODO: Camera Code
-        # pygame.camera.init()
 
         self.run_environment()
         pass
 
     @staticmethod
     def distance(pts1, pts2):
-        return np.linalg.norm([pts1, pts2])
+        return (pts1[0] - pts2[0]) ** 2 + (pts1[1] - pts2[1]) ** 2
 
     def create_blocks(self, num_blocks=2, block_size=50):
-        for i in range(num_blocks):
-            self.blocks.add(Blocks(i, self.grid_centers))
-
-        # Give the surface a color to differentiate it from the background
-
+        for i, blockCenterIdx in enumerate(np.random.choice(len(self.grid_centers), num_blocks, replace=False)):
+            self.blocks.add(Blocks(i, self.grid_centers[blockCenterIdx]))
         pygame.display.flip()
 
     def move_block(self, action, drag, rectangle, sel_block_id):
         if action == K_UP:
-            this_actions = actions_taken['MOVE_UP']
+            this_action = 'MOVE_UP'
             dx, dy = 0, -5
         elif action == K_DOWN:
-            this_actions = actions_taken['MOVE_DOWN']
+            this_action = 'MOVE_DOWN'
             dx, dy = 0, 5
         elif action == K_LEFT:
-            this_actions = actions_taken['MOVE_LEFT']
+            this_action = 'MOVE_LEFT'
             dx, dy = -5, 0
         elif action == K_RIGHT:
-            this_actions = actions_taken['MOVE_RIGHT']
+            this_action = 'MOVE_RIGHT'
             dx, dy = 5, 0
         else:
             raise IOError("Invalid Action", action)
 
         if drag:
-            other_blocks = [bi for bi in self.blocks if bi.rect != rectangle]
-            collision_indices = rectangle.collidelistall(other_blocks)
-            if collision_indices:
-                for ind in collision_indices:
-                    if BlockWorld.distance(other_blocks[ind].rect.center, rectangle.center) <= \
-                        BlockWorld.distance(
-                            (other_blocks[ind].rect.centerx + dx, other_blocks[ind].rect.centery + dy),
-                            rectangle.center):
-                        break
-                else:
-                    this_actions.append((time.time() - self.timer_start, sel_block_id))
-                    rectangle.centerx += dx
-                    rectangle.centery += dy
-            elif rectangle:
-                this_actions.append((time.time() - self.timer_start, sel_block_id))
+            all_dists = [
+                BlockWorld.distance((rectangle.centerx + dx, rectangle.centery + dy), other_block.rect.center) >= 2500
+                for other_block in self.blocks if other_block.rect != rectangle]
+            if all(all_dists):
+                actions_taken.append((this_action, sel_block_id))
                 rectangle.centerx += dx
                 rectangle.centery += dy
 
@@ -109,10 +83,6 @@ class BlockWorld:
         rectangle = None
         sel_block_id = None
         prev_action = None
-
-        # TODO: Camera Code
-        # cam = pygame.camera.Camera("/dev/video0", (640, 480))
-        # cam.start()
 
         while running:
             time.sleep(1. / 25)
@@ -131,7 +101,7 @@ class BlockWorld:
 
                     elif event.key == K_RSHIFT:
                         prev_action = None
-                        actions_taken['FINISHED'].append(time.time() - self.timer_start)
+                        actions_taken.append(('FINISHED', None))
                         print("Finished Demonstration")
                         print(actions_taken)
 
@@ -139,25 +109,16 @@ class BlockWorld:
                         print("Dropped")
                         drag = False
                         rectangle = None
-                        actions_taken['DROP'].append((time.time() - self.timer_start, sel_block_id))
+                        actions_taken.append(('DROP', sel_block_id))
                         pass
 
-                    elif event.key == K_UP:
-                        prev_action = K_UP
-
-                    elif event.key == K_DOWN:
-                        prev_action = K_DOWN
-
-                    elif event.key == K_LEFT:
-                        prev_action = K_LEFT
-
-                    elif event.key == K_RIGHT:
-                        prev_action = K_RIGHT
+                    elif event.key in set([K_UP, K_DOWN, K_LEFT, K_RIGHT]):
+                        prev_action = event.key
 
                 # Check for QUIT event; if QUIT, set running to false
                 elif event.type == QUIT:
                     prev_action = None
-                    actions_taken['FINISHED'].append(time.time() - self.timer_start)
+                    actions_taken.append(('FINISHED', None))
                     print("Finished Demonstration")
                     print(actions_taken)
                     running = False
@@ -171,29 +132,8 @@ class BlockWorld:
                             drag = True
                             rectangle = block.rect
                             sel_block_id = block.id
-                            actions_taken['PICK'].append((time.time() - self.timer_start, event.pos, block.id))
+                            actions_taken.append(('PICK', block.id))
                             break
-
-                # elif event.type == pygame.MOUSEMOTION:
-                #     mouse_x, mouse_y = event.pos
-                #     if drag:
-                #         other_blocks=[b for b in self.blocks if b.rect != rectangle]
-                #         collision_indices=rectangle.collidelistall(other_blocks)
-                #         if len(collision_indices)!=0:
-                #             for ind in collision_indices:
-                #                 if self.distance((other_blocks[ind].rect.centerx,other_blocks[ind].rect.centery),event.pos)<\
-                #                         self.distance((other_blocks[ind].rect.centerx,other_blocks[ind].rect.centery),(rectangle.centerx,rectangle.centery)):
-                #                         break
-                #             else:
-                #                 actions_taken['DRAG'].append((time.time() - self.timer_start, event.pos,sel_block_id))
-                #                 rectangle.centerx = mouse_x
-                #                 rectangle.centery = mouse_y
-                #         elif rectangle:
-                #             actions_taken['DRAG'].append((time.time() - self.timer_start, event.pos,sel_block_id))
-                #             rectangle.centerx = mouse_x
-                #             rectangle.centery = mouse_y
-                #     else:
-                #         actions_taken['MOVE'].append((time.time()-self.timer_start,event.pos,sel_block_id))
 
             if prev_action:
                 self.move_block(prev_action, drag, rectangle, sel_block_id)
@@ -202,8 +142,9 @@ class BlockWorld:
 
             pygame.display.flip()
 
-            filename = "./screen_capture/%d.png" % time.time()
-            pygame.image.save(self.screen, filename)
+            if self.record:
+                filename = "./screen_capture/%d.png" % time.time()
+                pygame.image.save(self.screen, filename)
 
 
-b = BlockWorld(1000, 950, 5)
+b = BlockWorld(1000, 950, 5, record=False)
